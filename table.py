831c410a3285381987b029f00eae8f488628c20f
Benjamin Gmeiner <benjamin.gmeiner@mailbox.org>
2017-03-13 15:32:53 +0100

    Add simple command sending

--- table.py
+++ table.py
@@ -20,7 +20,9 @@ class SerialConnection:
 
     def connect(self):
         """ """
-        self.serial_connection = serial.Serial(self.port, self.baudrate)
+        self.serial_connection = serial.Serial(self.port, self.baudrate, timeout=self.timeout)
+        print(self.serial_connection.readlines())
+        print("Connected to serial port")
 
     def disconnect(self):
         """ """
@@ -28,5 +30,11 @@ class SerialConnection:
 
     def command(self, com):
         """ """
-        self.serial_connection.write()
-        response = self.serial_connection.readlines()
+        self.serial_connection.write(com.encode('ascii') + b"\n")
+        response = self.serial_connection.readline().decode('ascii').strip("\r\n")
+        if "ok" in response:
+            return response
+        else:
+            return "ERROR: {}".format(response)
+
+
